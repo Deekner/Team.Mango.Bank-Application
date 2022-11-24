97280b3e759addde640c73e1e8698a8b4e41efa5
Dennis Ekner <114058184+Deekner@users.noreply.github.com>
2022-11-24 15:04:33 +0100

    Login hiarchy update

--- Team.Mango.Bank-Application/Program.cs
+++ Team.Mango.Bank-Application/Program.cs
@@ -7,7 +7,9 @@ namespace Team.Mango.Bank_Application
         static void Main(string[] args)
         {
             LogIn.Accounts();
-           
+
+
+            
 
             Console.WriteLine("Test Test Test From Dennis");
 
