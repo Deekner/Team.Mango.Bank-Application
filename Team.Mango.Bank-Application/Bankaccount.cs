3173093b27198343ea8559730a1208f3b35d1e1c
Tim Nilsson <60481888+BKKinvader@users.noreply.github.com>
2022-11-30 19:11:42 +0100

    Fixing issue merge
    Merge problem

---
+++ Team.Mango.Bank-Application/Bankaccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Team.Mango.Bank_Application
+{
+    internal class Bankaccount : Menu
+    {
+
+        public static void bankaccount(Users Currentuser)
+        {
+            Console.Clear();
+            Console.WriteLine("Which of your following bank account balance would you like to select?\n");
+            Console.WriteLine("[1]. Accounts Balance\n[2]. Saving Balance\n");
+            Console.WriteLine("[3]. Return to Main Menu");
+            Console.WriteLine();
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Your current balance is: {0}\n", Currentuser.balance());
+                    Console.WriteLine("Press [Enter] to return");
+                    Console.ReadLine();
+                    bankaccount(Currentuser);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Your current savings balance is: {0}\n", Currentuser.Savings());
+                    Console.WriteLine("Press [Enter] to return");
+                    Console.ReadLine();
+                    bankaccount(Currentuser);
+                    break;
+
+                default:
+                    Console.WriteLine("Press [Enter] to return to Main Menu...");
+                    break;
+            }
+
+            Console.Clear();
+        }
+
+
+    }
+}
