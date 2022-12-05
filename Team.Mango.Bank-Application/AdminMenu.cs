4ea444f409271b6ffb42752f5848dcb5d682ae4e
Tim Nilsson <60481888+BKKinvader@users.noreply.github.com>
2022-12-05 13:59:28 +0100

    InternalTransferFunction
    -Adding internalTransferFuction
    -Change position of Adminmenu to a class named AdminMenu insted of in login class.

---
+++ Team.Mango.Bank-Application/AdminMenu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Team.Mango.Bank_Application
+{
+    public class AdminMenu
+    {
+        //ADMIN Menu
+        public void adminMenu(List<User> Users, User CurrentUser)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("             MAIN MENU             ");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("|    [1.]  Show All Users            | ");
+                Console.WriteLine("|    [2.]  Create Account            | ");
+                Console.WriteLine("|    [3.]  Logout                    | ");
+                Console.WriteLine("|    [4.]  Exit application          | ");
+                Console.WriteLine("--------------------------------------");
+
+
+                try
+                {
+                    int choice = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            CurrentUser.ShowAllUsers(Users, CurrentUser);
+                            break;
+
+                        case 2:
+                            // Sending list of Users and CurrentUser and if Admin is true
+                            User.CreateUser(Users, CurrentUser, true);
+                            break;
+
+                        case 3:
+                            Login logout = new Login();
+                            logout.UserLogin(Users);
+                            break;
+
+
+                        case 4:
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Input!");
+                    Console.ReadLine();
+                }
+            } while (true);
+        }
+
+
+    }
+}
